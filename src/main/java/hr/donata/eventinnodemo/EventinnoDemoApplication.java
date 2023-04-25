d9236ed6ee2fbd8f2f1fa89ca945141012f77943
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-04-25 09:05:20 +0200

    Initial commit

---
+++ src/main/java/hr/donata/eventinnodemo/EventinnoDemoApplication.java
@@ -0,0 +1,13 @@
+package hr.donata.eventinnodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventinnoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventinnoDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/donata/eventinnodemo/EventinnoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.donata.eventinnodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventinnoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
