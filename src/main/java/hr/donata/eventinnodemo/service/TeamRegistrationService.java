3451f4db5e3c8b699f4e11b9d9645b4a27d35b90
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-09 09:26:54 +0200

    Deleting unused import

--- src/main/java/hr/donata/eventinnodemo/service/TeamRegistrationService.java
+++ src/main/java/hr/donata/eventinnodemo/service/TeamRegistrationService.java
@@ -2,7 +2,6 @@ package hr.donata.eventinnodemo.service;
 
 import hr.donata.eventinnodemo.dto.EventDto;
 import hr.donata.eventinnodemo.dto.TeamRegistrationDto;
-import hr.donata.eventinnodemo.entity.TeamRegistration;
 
 public interface TeamRegistrationService {
 
