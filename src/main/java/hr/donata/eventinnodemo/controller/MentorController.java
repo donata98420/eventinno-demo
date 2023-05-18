84fd91c25fdf78e737000eb35d84356daead81e5
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-18 13:36:37 +0200

    Changing method from private to public

--- src/main/java/hr/donata/eventinnodemo/controller/MentorController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/MentorController.java
@@ -1,6 +1,7 @@
 package hr.donata.eventinnodemo.controller;
 
 import hr.donata.eventinnodemo.dto.MentorDto;
+import hr.donata.eventinnodemo.mapper.MentorMapper;
 import hr.donata.eventinnodemo.service.MentorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,20 +11,22 @@ import org.springframework.web.bind.annotation.*;
 public class MentorController {
 
     private final MentorService mentorService;
+    private final MentorMapper mentorMapper;
 
-    public MentorController(MentorService mentorService) {
+    public MentorController(MentorService mentorService, MentorMapper mentorMapper) {
 
         this.mentorService = mentorService;
+        this.mentorMapper = mentorMapper;
     }
 
     @PostMapping(path = "/save")
-    private ResponseEntity<MentorDto> saveMentor(@RequestBody MentorDto mentorDto) {
+    public ResponseEntity<MentorDto> saveMentor(@RequestBody MentorDto mentorDto) {
         mentorService.create(mentorDto);
         return ResponseEntity.ok(mentorDto);
     }
 
     @DeleteMapping(path = "/delete/{id}")
-    private ResponseEntity<String> deleteMentor(@PathVariable Long id) {
+    public ResponseEntity<String> deleteMentor(@PathVariable Long id) {
         mentorService.deleteMentor(id);
         return ResponseEntity.ok("You deleted one mentor from database.");
     }
