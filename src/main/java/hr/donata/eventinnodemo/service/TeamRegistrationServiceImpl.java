67ac227b84491ebda81930ef4fb39fc9e3b728b7
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-22 12:16:44 +0200

    Adding 2 entities

--- src/main/java/hr/donata/eventinnodemo/service/TeamRegistrationServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/TeamRegistrationServiceImpl.java
@@ -11,7 +11,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
 import java.util.List;
 
 @Service
