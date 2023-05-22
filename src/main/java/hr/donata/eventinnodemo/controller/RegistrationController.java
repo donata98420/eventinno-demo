0be5067e9b9c2b6e7669dab3f6ab672f58efe4e9
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-22 12:50:21 +0200

    Adding method for DELETE request

--- src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
@@ -4,10 +4,7 @@ import hr.donata.eventinnodemo.dto.RegistrationDto;
 import hr.donata.eventinnodemo.dto.UserDto;
 import hr.donata.eventinnodemo.service.RegistrationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/registration")
@@ -24,6 +21,12 @@ public class RegistrationController {
     private ResponseEntity<RegistrationDto> saveRegistration(@RequestBody RegistrationDto registrationDto) {
         registrationService.create(registrationDto);
         return ResponseEntity.ok(registrationDto);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    private ResponseEntity<String> deleteRegistration(@PathVariable Long id) {
+        registrationService.deleteRegistration(id);
+        return ResponseEntity.ok("You deleted one registration from the database.");
     }
 
 }
--- src/main/java/hr/donata/eventinnodemo/service/RegistrationService.java
+++ src/main/java/hr/donata/eventinnodemo/service/RegistrationService.java
@@ -4,8 +4,5 @@ import hr.donata.eventinnodemo.dto.RegistrationDto;
 
 public interface RegistrationService {
     void create(RegistrationDto registrationDto);
-
-
-
-
+    void deleteRegistration(Long id);
 }
