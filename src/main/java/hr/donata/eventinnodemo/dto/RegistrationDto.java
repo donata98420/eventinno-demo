7848e25b190e46355ba64dd99b09a750452f6d07
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-24 10:46:29 +0200

    Adding dto part

--- src/main/java/hr/donata/eventinnodemo/dto/RegistrationDto.java
+++ src/main/java/hr/donata/eventinnodemo/dto/RegistrationDto.java
@@ -1,4 +1,33 @@
 package hr.donata.eventinnodemo.dto;
 
+import java.util.UUID;
+
 public class RegistrationDto {
+    private Long id;
+    private UUID uuid;
+    private UserDto user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public UserDto getUser() {
+        return user;
+    }
+
+    public void setUser(UserDto user) {
+        this.user = user;
+    }
 }
