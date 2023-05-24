03f9de53ddb9f6130f9651d303857a650de88987
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-24 11:32:37 +0200

    Adding methods

--- src/main/java/hr/donata/eventinnodemo/service/NameService.java
+++ src/main/java/hr/donata/eventinnodemo/service/NameService.java
@@ -1,7 +1,11 @@
 package hr.donata.eventinnodemo.service;
 
+import hr.donata.eventinnodemo.dto.NameDto;
+
 public interface NameService {
 
 
+    void create(NameDto nameDto);
 
+    void deleteName(Long id);
 }
--- src/main/java/hr/donata/eventinnodemo/service/NameServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/NameServiceImpl.java
@@ -1,8 +1,18 @@
 package hr.donata.eventinnodemo.service;
 
 
+import hr.donata.eventinnodemo.dto.NameDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NameServiceImpl implements NameService {
+    @Override
+    public void create(NameDto nameDto) {
+
+    }
+
+    @Override
+    public void deleteName(Long id) {
+
+    }
 }
