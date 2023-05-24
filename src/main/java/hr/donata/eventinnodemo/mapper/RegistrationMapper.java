d15384a95005ad717ca9e0e6607070d01d798d91
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-24 10:40:09 +0200

    Adding create method

--- src/main/java/hr/donata/eventinnodemo/mapper/RegistrationMapper.java
+++ src/main/java/hr/donata/eventinnodemo/mapper/RegistrationMapper.java
@@ -1,4 +1,8 @@
 package hr.donata.eventinnodemo.mapper;
 
+import hr.donata.eventinnodemo.dto.RegistrationDto;
+import hr.donata.eventinnodemo.entity.Registration;
+
 public interface RegistrationMapper {
+    Registration registrationDtoToRegistration(RegistrationDto registrationDto);
 }
--- src/main/java/hr/donata/eventinnodemo/service/RegistrationServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/RegistrationServiceImpl.java
@@ -1,19 +1,31 @@
 package hr.donata.eventinnodemo.service;
 
 import hr.donata.eventinnodemo.dto.RegistrationDto;
+import hr.donata.eventinnodemo.entity.Registration;
+import hr.donata.eventinnodemo.mapper.RegistrationMapper;
+import hr.donata.eventinnodemo.repository.RegistrationRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class RegistrationServiceImpl implements RegistrationService{
+@Transactional
+public class RegistrationServiceImpl implements RegistrationService {
+    private final RegistrationRepository registrationRepository;
+    private final RegistrationMapper registrationMapper;
 
+    public RegistrationServiceImpl(RegistrationRepository registrationRepository, RegistrationMapper registrationMapper) {
+        this.registrationRepository = registrationRepository;
+        this.registrationMapper = registrationMapper;
+    }
 
     @Override
     public void create(RegistrationDto registrationDto) {
-
+        Registration registration = registrationMapper.registrationDtoToRegistration(registrationDto);
+        registrationRepository.save(registration);
     }
 
     @Override
     public void deleteRegistration(Long id) {
-
+        registrationRepository.deleteById(id);
     }
 }
