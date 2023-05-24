0b48b3d15426f1cc87d5fc797a870601d70064a3
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-24 11:40:26 +0200

    Adding repository, mapper and service

--- src/main/java/hr/donata/eventinnodemo/service/NameServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/NameServiceImpl.java
@@ -2,10 +2,24 @@ package hr.donata.eventinnodemo.service;
 
 
 import hr.donata.eventinnodemo.dto.NameDto;
+import hr.donata.eventinnodemo.mapper.NameMapper;
+import hr.donata.eventinnodemo.repository.NameRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NameServiceImpl implements NameService {
+
+    private final NameRepository nameRepository;
+    private final NameMapper nameMapper;
+    private final NameService nameService;
+
+    public NameServiceImpl(NameRepository nameRepository, NameMapper nameMapper, NameService nameService) {
+        this.nameRepository = nameRepository;
+        this.nameMapper = nameMapper;
+        this.nameService = nameService;
+    }
+
+
     @Override
     public void create(NameDto nameDto) {
 
