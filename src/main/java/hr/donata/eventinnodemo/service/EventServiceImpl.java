6bf08cc61fdf75e1019b983de7425a222ac149cc
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-27 12:21:24 +0200

    Removing duplicate throw block

--- src/main/java/hr/donata/eventinnodemo/service/EventServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/EventServiceImpl.java
@@ -11,15 +11,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.time.LocalDateTime;
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class EventServiceImpl implements EventService{
+public class EventServiceImpl implements EventService {
     private final EventRepository eventRepository;
     private final EventMapper eventMapper;
     private final TeamRegistrationService teamRegistrationService;
@@ -29,11 +30,10 @@ public class EventServiceImpl implements EventService{
     public void create(EventDto eventDto) {
         try {
             if (eventRepository.findByName(eventDto.getName()).isPresent()) {
-                throw new BadRequestException("Sorry, event with this name already exists.");
+                throw new BadRequestException("Sorry, this event name already exists. Use another one.");
             }
 
             if (eventDto.getRegistrationsNotBefore() != null && eventDto.getRegistrationsNotAfter() != null) {
-
                 LocalDateTime now = LocalDateTime.now();
                 LocalDateTime registrationsNotBefore = eventDto.getRegistrationsNotBefore();
                 LocalDateTime registrationsNotAfter = eventDto.getRegistrationsNotAfter();
@@ -57,29 +57,27 @@ public class EventServiceImpl implements EventService{
                 teamRegistrationService.create(teamRegistrationDto);
             }
         } catch (DataIntegrityViolationException e) {
-            throw new BadRequestException("This event name already exists. Try with another one.");
-        } catch (BadRequestException e) {
-
-            throw e;
-
+            throw new RuntimeException("This event name already exists. Try with another one.", e);
         } catch (Exception e) {
             throw new RuntimeException("Oops. An unexpected error occurred.", e);
         }
     }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public static class BadRequestException extends IllegalArgumentException {
         private String errorMessage;
 
-        public BadRequestException(String message, String errorMessage) {
+        public BadRequestException(String message) {
             super(message);
             this.errorMessage = errorMessage;
         }
+
         public String getErrorMessage() {
             return errorMessage;
         }
     }
 
-    // if event does not accept registrations
+    // If event does not accept registrations
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     public static class MethodNotAllowedException extends RuntimeException {
         public MethodNotAllowedException(String message, String eventName) {
@@ -89,7 +87,6 @@ public class EventServiceImpl implements EventService{
 
     @Override
     public void deleteEvent(Long id) {
-
         eventRepository.deleteById(id);
     }
 }
