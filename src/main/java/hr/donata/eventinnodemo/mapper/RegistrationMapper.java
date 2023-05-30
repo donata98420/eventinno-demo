deb6ee64d6c88e43732bbd35198ea32ce6a3e82b
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-30 23:26:11 +0200

    Mapping LocalDateTime

--- src/main/java/hr/donata/eventinnodemo/mapper/RegistrationMapper.java
+++ src/main/java/hr/donata/eventinnodemo/mapper/RegistrationMapper.java
@@ -5,11 +5,20 @@ import hr.donata.eventinnodemo.entity.Registration;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+
 @Mapper(componentModel = "spring")
 public interface RegistrationMapper {
 
     RegistrationMapper INSTANCE = Mappers.getMapper(RegistrationMapper.class);
     Registration registrationDtoToRegistration(RegistrationDto registrationDto);
 
+    default LocalDateTime mapChronoLocalDateTime(ChronoLocalDateTime<?> chronoLocalDateTime) {
+        if (chronoLocalDateTime == null) {
+            return null;
+        }
+        return LocalDateTime.from(chronoLocalDateTime);
+    }
 
 }
