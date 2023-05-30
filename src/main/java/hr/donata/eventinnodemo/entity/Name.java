04ede42e6fc807e01501a219f92f21988cd74372
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-31 00:01:45 +0200

    Adding @Columns

--- src/main/java/hr/donata/eventinnodemo/entity/Name.java
+++ src/main/java/hr/donata/eventinnodemo/entity/Name.java
@@ -1,5 +1,6 @@
 package hr.donata.eventinnodemo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -24,7 +25,9 @@ public class Name {
         this.last = last;
     }
 
+    @Column(name ="first")
     private String first;
+    @Column(name ="last")
     private String last;
 
     public Long getOib() {
