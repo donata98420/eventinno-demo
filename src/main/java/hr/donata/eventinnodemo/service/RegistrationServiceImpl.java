8d11b018ca4e3ae2f274d450b892242b61e0d2d2
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-05-30 23:38:52 +0200

    Wrapping ZoneDateTime

--- src/main/java/hr/donata/eventinnodemo/service/RegistrationServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/RegistrationServiceImpl.java
@@ -6,20 +6,17 @@ import hr.donata.eventinnodemo.entity.Registration;
 import hr.donata.eventinnodemo.mapper.RegistrationMapper;
 import hr.donata.eventinnodemo.repository.RegistrationRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
-import java.time.chrono.ChronoLocalDateTime;
 import java.util.UUID;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class RegistrationServiceImpl implements RegistrationService {
+
     private final RegistrationRepository registrationRepository;
     private final RegistrationMapper registrationMapper;
 
@@ -30,19 +27,25 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public void create(RegistrationDto registrationDto, EventDto eventDto) {
-        LocalDateTime now = LocalDateTime.now();
+        ZonedDateTime now = ZonedDateTime.now();
 
+        // Check if the registration is within the specified time constraints
         if (registrationDto.getRegistrationsNotBefore() != null && registrationDto.getRegistrationsNotAfter() != null) {
             ZonedDateTime registrationsNotBefore = ZonedDateTime.from(registrationDto.getRegistrationsNotBefore());
             ZonedDateTime registrationsNotAfter = ZonedDateTime.from(registrationDto.getRegistrationsNotAfter());
 
-            if (now.isBefore(ChronoLocalDateTime.from(registrationsNotBefore)) || now.isAfter(ChronoLocalDateTime.from(registrationsNotAfter))) {
+            if (now.isBefore(registrationsNotBefore) || now.isAfter(registrationsNotAfter)) {
                 throw new MethodNotAllowedException("Registrations for this event are currently closed.", eventDto != null ? eventDto.getName() : "");
             }
         }
 
+        // Generate a UUID for the registration
         registrationDto.setUuid(UUID.randomUUID());
+
+        // Map the RegistrationDto to the Registration entity
         Registration registration = registrationMapper.registrationDtoToRegistration(registrationDto);
+
+        // Save the registration in the repository
         registrationRepository.save(registration);
     }
 
@@ -51,7 +54,6 @@ public class RegistrationServiceImpl implements RegistrationService {
         registrationRepository.deleteById(id);
     }
 
-    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     public static class MethodNotAllowedException extends RuntimeException {
         public MethodNotAllowedException(String message, String name) {
             super(message + (name.isEmpty() ? "" : " Event: " + name));
