e330f9d7e4f201d49a12d7a3ee6bec67ca69a6cf
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-06 09:59:06 +0200

    Adding @AfterMapping

--- src/main/java/hr/donata/eventinnodemo/mapper/EventMapper.java
+++ src/main/java/hr/donata/eventinnodemo/mapper/EventMapper.java
@@ -2,7 +2,10 @@ package hr.donata.eventinnodemo.mapper;
 
 import hr.donata.eventinnodemo.dto.EventDto;
 import hr.donata.eventinnodemo.entity.Event;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 @Mapper(componentModel = "spring")
 public interface EventMapper {
@@ -10,5 +13,8 @@ public interface EventMapper {
     EventMapper INSTANCE = Mappers.getMapper(EventMapper.class);
     Event eventDtoToEvent(EventDto eventDto);
 
-
+    @AfterMapping
+    default void mapEventIdInUser(@MappingTarget Event event) {
+        event.getTeamRegistrationList().forEach(team -> team.setEvent(event));
+    }
 }
