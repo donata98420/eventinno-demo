4453e08264feb341e7c442d701cb5e4edfc6883a
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-07 13:36:55 +0200

    Adding String

--- src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
@@ -4,24 +4,27 @@ import hr.donata.eventinnodemo.dto.RegistrationDto;
 import hr.donata.eventinnodemo.service.RegistrationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/event/{event_id}/registrations")
 public class RegistrationController {
     private final RegistrationService registrationService;
+
     public RegistrationController(RegistrationService registrationService) {
         this.registrationService = registrationService;
     }
+
     @PostMapping(path = "")
-    private ResponseEntity<RegistrationDto> saveRegistration(@RequestBody RegistrationDto registrationDto) {
-        registrationService.create(registrationDto);
-        return ResponseEntity.ok(registrationDto);
+    public ResponseEntity<RegistrationDto> saveRegistration(@RequestBody RegistrationDto registrationDto) {
+        ResponseEntity<String> registration = registrationService.create(registrationDto);
+        return ResponseEntity.created(URI.create("/registration/" + registration.getBody()))
+                .body(registrationDto);
     }
 
     @DeleteMapping(path = "/delete/{id}")
-    private ResponseEntity<String> deleteRegistration(@PathVariable Long id) {
+    public ResponseEntity<String> deleteRegistration(@PathVariable Long id) {
         registrationService.deleteRegistration(id);
         return ResponseEntity.ok("You deleted one registration from the database.");
     }
-
 }
