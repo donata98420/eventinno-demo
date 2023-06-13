fe5bc6a02c734adb6d2451efa8856a7317357b53
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-13 09:10:55 +0200

    Removing getters and setters

--- src/main/java/hr/donata/eventinnodemo/dto/EducationDto.java
+++ src/main/java/hr/donata/eventinnodemo/dto/EducationDto.java
@@ -8,27 +8,5 @@ public class EducationDto {
     private int year;
     private Long id;
 
-    public String getFaculty() {
-        return faculty;
-    }
 
-    public void setFaculty(String faculty) {
-        this.faculty = faculty;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
