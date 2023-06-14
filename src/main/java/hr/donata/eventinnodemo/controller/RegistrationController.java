cf7518d7d180277de8772d99e8a6eb0559f54fc3
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-14 10:09:11 +0200

    Adding eventId to path variable

--- src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
@@ -1,14 +1,12 @@
 package hr.donata.eventinnodemo.controller;
 
-import hr.donata.eventinnodemo.dto.MentorDto;
 import hr.donata.eventinnodemo.dto.RegistrationDto;
 import hr.donata.eventinnodemo.service.RegistrationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.net.URI;
 
 @RestController
-@RequestMapping("/event/{event_id}/registrations")
+@RequestMapping("/event")
 public class RegistrationController {
     private final RegistrationService registrationService;
 
@@ -16,14 +14,14 @@ public class RegistrationController {
         this.registrationService = registrationService;
     }
 
-    @PostMapping(path = "/save")
-    private ResponseEntity<RegistrationDto> saveRegistration(@RequestBody RegistrationDto registrationDto) {
-        registrationService.create(registrationDto);
-        return ResponseEntity.ok(registrationDto);
+    @PostMapping(path = "/{event_id}/registrations")
+    public ResponseEntity<String> saveRegistration(@RequestBody RegistrationDto registrationDto, @PathVariable("event_id") Long eventId) {
+        ResponseEntity<String> response = registrationService.create(registrationDto, eventId);
+        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
     }
 
-    @DeleteMapping(path = "/{id}")
-    public ResponseEntity<String> deleteRegistration(@PathVariable Long id) {
+    @DeleteMapping(path = "/registrations/{id}")
+    public ResponseEntity<String> deleteRegistration(@PathVariable("id") Long id) {
         registrationService.deleteRegistration(id);
         return ResponseEntity.ok("You deleted one registration from the database.");
     }
