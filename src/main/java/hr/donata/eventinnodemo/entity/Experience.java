a23d7167501e9dc0340317928039910e3b0cf5f0
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-14 13:27:23 +0200

    Adding columns

--- src/main/java/hr/donata/eventinnodemo/entity/Experience.java
+++ src/main/java/hr/donata/eventinnodemo/entity/Experience.java
@@ -1,10 +1,16 @@
 package hr.donata.eventinnodemo.entity;
 
+import jakarta.persistence.Column;
+
 public class Experience {
 
+    @Column(name ="years")
     private int years;
+    @Column(name ="skills")
     private String[] skills;
+    @Column(name ="repositoryUrl")
     private String repositoryUrl;
+    @Column(name ="summary")
     private String summary;
 
 }
