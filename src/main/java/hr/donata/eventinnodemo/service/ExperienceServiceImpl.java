d19bbf9a8a25275250d9e37a83f823cd76f3532f
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-14 13:36:21 +0200

    Adding annotations, repository and mapper

--- src/main/java/hr/donata/eventinnodemo/service/ExperienceServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/ExperienceServiceImpl.java
@@ -1,4 +1,15 @@
 package hr.donata.eventinnodemo.service;
 
+import hr.donata.eventinnodemo.mapper.ExperienceMapper;
+import hr.donata.eventinnodemo.repository.ExperienceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ExperienceServiceImpl {
+
+    private final ExperienceRepository experienceRepository;
+    private final ExperienceMapper experienceMapper;
+
 }
