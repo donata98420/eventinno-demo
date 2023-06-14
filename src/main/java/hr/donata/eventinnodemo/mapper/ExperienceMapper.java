62a5c0f766b6432ec66227a7cb6ebe00be3323f1
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-14 13:32:32 +0200

    Adding mapper

--- src/main/java/hr/donata/eventinnodemo/mapper/ExperienceMapper.java
+++ src/main/java/hr/donata/eventinnodemo/mapper/ExperienceMapper.java
@@ -1,4 +1,12 @@
 package hr.donata.eventinnodemo.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface ExperienceMapper {
+
+    ExperienceMapper INSTANCE = Mappers.getMapper(ExperienceMapper.class);
+
+
 }
