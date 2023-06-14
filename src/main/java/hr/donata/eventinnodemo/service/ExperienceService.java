f725974eac4818c6ff22c40c5e62f9657a98d53f
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-14 13:30:20 +0200

    Adding create and delete methods

--- src/main/java/hr/donata/eventinnodemo/service/ExperienceService.java
+++ src/main/java/hr/donata/eventinnodemo/service/ExperienceService.java
@@ -1,4 +1,9 @@
 package hr.donata.eventinnodemo.service;
 
+import hr.donata.eventinnodemo.dto.ExperienceDto;
+
 public interface ExperienceService {
+
+    void create(ExperienceDto experienceDto);
+    void deleteExperience(Long id);
 }
