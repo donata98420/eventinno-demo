1b627977269aa817ceaa335daee2fb51d08a6afd
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-15 12:20:59 +0200

    Removing imports

--- src/main/java/hr/donata/eventinnodemo/service/PersonalServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/PersonalServiceImpl.java
@@ -1,16 +1,15 @@
 package hr.donata.eventinnodemo.service;
 
 import hr.donata.eventinnodemo.dto.PersonalDto;
+import hr.donata.eventinnodemo.entity.Personal;
 import hr.donata.eventinnodemo.mapper.PersonalMapper;
 import hr.donata.eventinnodemo.repository.PersonalRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonalServiceImpl implements PersonalService {
-
     private final PersonalMapper personalMapper;
     private final PersonalRepository personalRepository;
-
     public PersonalServiceImpl(PersonalMapper personalMapper, PersonalRepository personalRepository) {
         this.personalMapper = personalMapper;
         this.personalRepository = personalRepository;
@@ -18,7 +17,8 @@ public class PersonalServiceImpl implements PersonalService {
 
     @Override
     public void create(PersonalDto personalDto) {
-
+        Personal personal = personalMapper.personalDtoToPersonal(personalDto);
+        personalRepository.save(personal);
     }
 
     @Override
