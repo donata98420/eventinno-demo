3766165b5d74494443309f09c94612329009aeaf
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-15 12:06:54 +0200

    Adding columns

--- src/main/java/hr/donata/eventinnodemo/entity/Personal.java
+++ src/main/java/hr/donata/eventinnodemo/entity/Personal.java
@@ -1,5 +1,6 @@
 package hr.donata.eventinnodemo.entity;
 
+import hr.donata.eventinnodemo.dto.RegistrationDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,5 +14,15 @@ public class Personal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name ="email")
+    private String email;
+    @Column(name ="phone")
+    private String phone;
+    @Column(name ="education")
+    private RegistrationDto.EducationDto education;
+    @Column(name ="summary")
+    private String summary;
+
 
 }
