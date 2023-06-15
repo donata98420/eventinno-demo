afcc4922836fe55085506d3dcea145e19c1b0934
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-15 09:05:26 +0200

    Adding methods

---
+++ src/main/java/hr/donata/eventinnodemo/service/PersonalServiceImpl.java
@@ -0,0 +1,28 @@
+package hr.donata.eventinnodemo.service;
+
+import hr.donata.eventinnodemo.dto.PersonalDto;
+import hr.donata.eventinnodemo.mapper.PersonalMapper;
+import hr.donata.eventinnodemo.repository.PersonalRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonalServiceImpl implements PersonalService {
+
+    private final PersonalMapper personalMapper;
+    private final PersonalRepository personalRepository;
+
+    public PersonalServiceImpl(PersonalMapper personalMapper, PersonalRepository personalRepository) {
+        this.personalMapper = personalMapper;
+        this.personalRepository = personalRepository;
+    }
+
+    @Override
+    public void create(PersonalDto personalDto) {
+
+    }
+
+    @Override
+    public void deletePersonal(Long id) {
+
+    }
+}
