1ec847fe485a9042676eee1485e740d09fa2f4c1
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-19 13:24:16 +0200

    Editing list

--- src/main/java/hr/donata/eventinnodemo/entity/Experience.java
+++ src/main/java/hr/donata/eventinnodemo/entity/Experience.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.util.List;
 
 @Entity
@@ -15,14 +14,13 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Experience {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name ="years")
     private int years;
     @Column(name ="skills")
-    List<String> skills
+    List<String> skills;
     @Column(name ="repositoryUrl")
     private String repositoryUrl;
     @Column(name ="summary")
