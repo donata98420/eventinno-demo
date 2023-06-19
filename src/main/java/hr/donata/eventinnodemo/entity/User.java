873e03f7ee0e60bed7681f78303d971a9ccf0d98
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-19 14:45:54 +0200

    Editing relationship

--- src/main/java/hr/donata/eventinnodemo/entity/User.java
+++ src/main/java/hr/donata/eventinnodemo/entity/User.java
@@ -33,8 +33,8 @@ public class User {
     private String motivation;
     private String preferredOS;
 
-    @OneToMany(mappedBy = "user", cascade = {CascadeType.ALL})
-    private List<Registration> registrationList;
+    //@OneToMany(mappedBy = "user", cascade = {CascadeType.ALL})
+   // private List<Registration> registrationList;
 
 
 }
