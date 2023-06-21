af0e807a11075512249e6e14364e1e4d70e086f7
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-21 12:58:37 +0200

    Editing path

--- src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/event")
+@RequestMapping("/registration")
 public class RegistrationController {
     private final RegistrationService registrationService;
 
