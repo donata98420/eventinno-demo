a492cf7da1110f1c7667d073fd33405a7f9b88d1
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-22 14:18:32 +0200

    Adding path for event that exists for specific registration

--- src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package hr.donata.eventinnodemo.controller;
 
 import hr.donata.eventinnodemo.dto.RegistrationDto;
+import hr.donata.eventinnodemo.service.EventService;
 import hr.donata.eventinnodemo.service.RegistrationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class RegistrationController {
     private final RegistrationService registrationService;
 
-    public RegistrationController(RegistrationService registrationService) {
+    public RegistrationController(RegistrationService registrationService, EventService eventService) {
         this.registrationService = registrationService;
     }
 
@@ -28,6 +29,8 @@ public class RegistrationController {
         try {
             registrationService.deleteRegistrationForEvent(registrationId, eventId);
             return ResponseEntity.ok("Registration is successfully deleted.");
+
+            //   /registration/{registrationId}/events/{eventId}
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
