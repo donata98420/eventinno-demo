60aeada5eeb32b66b90b1e02068ad2390ea0f195
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-28 10:03:54 +0200

    Adding annotations

--- src/main/java/hr/donata/eventinnodemo/service/ScoreServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/ScoreServiceImpl.java
@@ -1,7 +1,11 @@
 package hr.donata.eventinnodemo.service;
 
 import hr.donata.eventinnodemo.entity.Registration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class ScoreServiceImpl implements ScoreService{
     @Override
     public int calculateScore(Registration registration) {
