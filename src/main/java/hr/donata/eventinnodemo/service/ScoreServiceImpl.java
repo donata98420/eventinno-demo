00acc07b3bb6592a89e774d2e4668706aa207cd0
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-28 10:01:56 +0200

    Adding calculate method

---
+++ src/main/java/hr/donata/eventinnodemo/service/ScoreServiceImpl.java
@@ -0,0 +1,10 @@
+package hr.donata.eventinnodemo.service;
+
+import hr.donata.eventinnodemo.entity.Registration;
+
+public class ScoreServiceImpl implements ScoreService{
+    @Override
+    public int calculateScore(Registration registration) {
+        return 0;
+    }
+}
