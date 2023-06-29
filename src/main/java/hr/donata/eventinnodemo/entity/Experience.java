6519fc95f52b57a578f668dd25cb2d4082897895
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-29 09:39:39 +0200

    Adding method

--- src/main/java/hr/donata/eventinnodemo/entity/Experience.java
+++ src/main/java/hr/donata/eventinnodemo/entity/Experience.java
@@ -24,5 +24,8 @@ public class Experience {
 
 
     public int getEducationYears() {
+
+    }
+    public int getExperienceYears() {
     }
 }
--- src/main/java/hr/donata/eventinnodemo/service/ScoreServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/ScoreServiceImpl.java
@@ -4,6 +4,8 @@ import hr.donata.eventinnodemo.entity.Registration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ScoreServiceImpl implements ScoreService{
@@ -18,7 +20,32 @@ public class ScoreServiceImpl implements ScoreService{
     public int calculateScore(Registration registration) {
 
         int score = 0;
-        // .............
+
+        //  for education = 2 points
+        int educationPoints = registration.getExperience().getEducationYears() * 2;
+        score += educationPoints;
+
+        // for experience = 5 points
+        int experiencePoints = registration.getExperience().getExperienceYears() * 5;
+        score += experiencePoints;
+
+        // points for skills = 20 points for Java, Spring, SB
+        // = other skills = Hibernate, JPA, Scala = 10 points
+        score += calculateSkillsPoints(registration.getExperience().getSkills());
+
+        // points for repository URL = 10 points
+        if (registration.getExperience().getRepositoryUrl() != null) {
+            score += 10;
+        }
+
+        return score;
+    }
+
+    private int calculateSkillsPoints(List<String> skills) {
+    }
+
+
+
 
     }
 }
