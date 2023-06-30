e7713ca3f39fcbc8f890160cec627da336590099
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-30 12:56:13 +0200

    Adding generated id and sequence

--- src/main/java/hr/donata/eventinnodemo/entity/ManualScore.java
+++ src/main/java/hr/donata/eventinnodemo/entity/ManualScore.java
@@ -1,4 +1,14 @@
 package hr.donata.eventinnodemo.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
 public class ManualScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "manualScore_sequence")
+    @SequenceGenerator(name = "manualScore_sequence", allocationSize = 1)
+    private Long id;
 }
