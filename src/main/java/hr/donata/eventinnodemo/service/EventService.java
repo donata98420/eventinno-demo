7a862eef2b9b56c44f3e70d14924cb84827fd210
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-30 08:12:13 +0200

    Adding method for GET mapping

--- src/main/java/hr/donata/eventinnodemo/service/EventService.java
+++ src/main/java/hr/donata/eventinnodemo/service/EventService.java
@@ -1,9 +1,13 @@
 package hr.donata.eventinnodemo.service;
 
 import hr.donata.eventinnodemo.dto.EventDto;
+import hr.donata.eventinnodemo.entity.Event;
+
+import java.util.Optional;
 
 public interface EventService {
 
     void create(EventDto eventDto);
     void deleteEvent(Long id);
+    Optional<Event> getEventById(Long id);
 }
