ac8c3362b464ffbc19c4e8479c2906cf037dc979
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-30 12:25:04 +0200

    Adding service and constructor

--- src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/RegistrationController.java
@@ -2,20 +2,21 @@ package hr.donata.eventinnodemo.controller;
 
 import hr.donata.eventinnodemo.dto.RegistrationDto;
 import hr.donata.eventinnodemo.entity.Registration;
-import org.springframework.http.HttpStatus;
+import hr.donata.eventinnodemo.service.EventService;
 import org.springframework.http.ResponseEntity;
 import hr.donata.eventinnodemo.service.RegistrationService;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/registration")
 public class RegistrationController {
     private final RegistrationService registrationService;
+    private final EventService eventService;
 
-    public RegistrationController(RegistrationService registrationService) {
+    public RegistrationController(RegistrationService registrationService, EventService eventService) {
         this.registrationService = registrationService;
+        this.eventService = eventService;
     }
 
     @PostMapping("/{event_id}/registrations")
@@ -37,6 +38,50 @@ public class RegistrationController {
         }
     }
 
+    @PutMapping("/{event_id}/registrations/{registration_id}/score")
+    private ResponseEntity<RegistrationDto> updateRegistrationScore(
+            @PathVariable("event_id") Long eventId,
+            @PathVariable("registration_id") Long registrationId,
+            @RequestBody RegistrationDto registrationDto
+    ) {
+        Optional<Event> eventOptional = eventService.getEventById(eventId);
+        Optional<Registration> registrationOptional = registrationService.getRegistrationById(registrationId);
+
+        if (eventOptional.isPresent() && registrationOptional.isPresent()) {
+            Event event = eventOptional.get();
+            Registration registration = registrationOptional.get();
+
+            // Updating
+            String score = registrationDto.getScore();
+            if (score.startsWith("+")) {
+                int pointsToAdd = Integer.parseInt(score.substring(1));
+                registration.setScore(registration.getScore() + pointsToAdd);
+
+            } else if (score.startsWith("-")) {
+                int pointsToSubtract = Integer.parseInt(score.substring(1));
+                registration.setScore(registration.getScore() - pointsToSubtract);
+            } else {
+
+                // HTTP 400
+                return ResponseEntity.badRequest().body(registrationDto);
+            }
+
+            // Possible comments
+            String comment = registrationDto.getComment();
+            if (comment != null && !comment.isEmpty()) {
+                registration.setComment(comment);
+            }
+
+            // Saving
+            registrationService.updateRegistration(registration);
+
+            // Returning updated
+            return ResponseEntity.ok(registrationDto);
+        } else {
+            // HTTP 404
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 
 
