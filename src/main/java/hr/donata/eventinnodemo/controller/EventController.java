2c24a10b98eff4b8d563b36df7e85021e51f9dab
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-30 12:23:49 +0200

    Removing PUT mapping

--- src/main/java/hr/donata/eventinnodemo/controller/EventController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/EventController.java
@@ -33,49 +33,4 @@ public class EventController {
         return ResponseEntity.ok("You deleted one event from the database.");
     }
 
-    @PutMapping("/{event_id}/registrations/{registration_id}/score")
-    private ResponseEntity<RegistrationDto> updateRegistrationScore(
-            @PathVariable("event_id") Long eventId,
-            @PathVariable("registration_id") Long registrationId,
-            @RequestBody RegistrationDto registrationDto
-    ) {
-        Optional<Event> eventOptional = eventService.getEventById(eventId);
-        Optional<Registration> registrationOptional = registrationService.getRegistrationById(registrationId);
-
-        if (eventOptional.isPresent() && registrationOptional.isPresent()) {
-            Event event = eventOptional.get();
-            Registration registration = registrationOptional.get();
-
-            // Updating
-            String score = registrationDto.getScore();
-            if (score.startsWith("+")) {
-                int pointsToAdd = Integer.parseInt(score.substring(1));
-                registration.setScore(registration.getScore() + pointsToAdd);
-
-            } else if (score.startsWith("-")) {
-                int pointsToSubtract = Integer.parseInt(score.substring(1));
-                registration.setScore(registration.getScore() - pointsToSubtract);
-            } else {
-
-                // HTTP 400
-                return ResponseEntity.badRequest().body(registrationDto);
-            }
-
-            // Possible comments
-            String comment = registrationDto.getComment();
-            if (comment != null && !comment.isEmpty()) {
-                registration.setComment(comment);
-            }
-
-            // Saving
-            registrationService.updateRegistration(registration);
-
-            // Returning updated
-            return ResponseEntity.ok(registrationDto);
-        } else {
-            // HTTP 404
-            return ResponseEntity.notFound().build();
-        }
-    }
-
 }
