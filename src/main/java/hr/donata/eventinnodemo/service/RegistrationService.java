bd0791860f49b9e024bdd3f4effa387c7993e2e2
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-30 14:15:43 +0200

    Adding void method

--- src/main/java/hr/donata/eventinnodemo/service/RegistrationService.java
+++ src/main/java/hr/donata/eventinnodemo/service/RegistrationService.java
@@ -1,13 +1,12 @@
 package hr.donata.eventinnodemo.service;
 
+import hr.donata.eventinnodemo.dto.ManualScoreDto;
 import hr.donata.eventinnodemo.dto.RegistrationDto;
-import hr.donata.eventinnodemo.entity.ManualScore;
 import org.springframework.http.ResponseEntity;
 
 public interface RegistrationService {
     ResponseEntity<String> create(RegistrationDto registrationDto, Long eventId);
     void deleteRegistration(Long id);
     void deleteRegistrationForEvent(Long registrationId, Long eventId);
-
-    void scoreRegistration(Long registrationId, ManualScore manualScore);
+    void scoreRegistration(Long registrationId, ManualScoreDto manualScore);
 }
