3fc51f6b4d350cbd928afd57064343172db49572
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-06-30 12:56:49 +0200

    Adding annotaations

--- src/main/java/hr/donata/eventinnodemo/entity/ManualScore.java
+++ src/main/java/hr/donata/eventinnodemo/entity/ManualScore.java
@@ -1,10 +1,17 @@
 package hr.donata.eventinnodemo.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "manuač_score")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ManualScore {
 
     @Id
