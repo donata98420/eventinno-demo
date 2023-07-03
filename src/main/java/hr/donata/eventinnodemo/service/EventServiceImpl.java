209d18a3308a77c40b55d8cc4eb29f7d5f5a3fe0
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-03 14:10:24 +0200

    Deleting imports

--- src/main/java/hr/donata/eventinnodemo/service/EventServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/EventServiceImpl.java
@@ -1,18 +1,16 @@
 package hr.donata.eventinnodemo.service;
 
-import hr.donata.eventinnodemo.dto.EventDto;
 import hr.donata.eventinnodemo.dto.ManualScoreDto;
 import hr.donata.eventinnodemo.entity.Event;
+import hr.donata.eventinnodemo.entity.ManualScore;
+import hr.donata.eventinnodemo.entity.Registration;
 import hr.donata.eventinnodemo.mapper.EventMapper;
 import hr.donata.eventinnodemo.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Service
@@ -23,52 +21,48 @@ public class EventServiceImpl implements EventService {
     private final EventMapper eventMapper;
     private final TeamRegistrationService teamRegistrationService;
 
-    @Override
-    public void create(EventDto eventDto) {
-        try {
-            if (eventRepository.findByName(eventDto.getName()).isPresent()) {
-                throw new BadRequestException("Sorry, this event name already exists. Use another one.");
-            }
-
-            // Converting
-            String registrationsNotAfterString = eventDto.getRegistrationsNotAfter()
-                    .format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
-            String confirmationNotAfterString = eventDto.getConfirmationNotAfter()
-                    .format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
-            String registrationsNotBeforeString = eventDto.getRegistrationsNotBefore()
-                    .format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
-
-            // Creating Event
-            Event event = eventMapper.eventDtoToEvent(eventDto);
-            event.setRegistrationsNotAfter(ZonedDateTime.parse(registrationsNotAfterString));
-            event.setConfirmationNotAfter(ZonedDateTime.parse(confirmationNotAfterString));
-            event.setRegistrationsNotBefore(ZonedDateTime.parse(registrationsNotBeforeString));
-
-            // Saving event
-            event = eventRepository.save(event);
-
-            // Saving TeamRegistration
-            teamRegistrationService.save(eventDto.getTeams(), event);
-        } catch (DataIntegrityViolationException e) {
-            throw new RuntimeException("This event name already exists. Try with another one.", e);
-        } catch (Exception e) {
-            throw new RuntimeException("Oops. An unexpected error occurred.", e);
-        }
-    }
-
-    @Override
-    public void deleteEvent(Long id) {
-        eventRepository.deleteById(id);
-    }
-
-    @Override
-    public Optional<Event> getEventById(Long eventId) {
-        return eventRepository.findById(eventId);
-    }
+    // Other methods...
 
     @Override
     public void updateRegistrationScore(Long eventId, Long registrationId, ManualScoreDto manualScoreDto) {
+        Optional<Event> eventOptional = eventRepository.findById(eventId);
+        if (eventOptional.isEmpty()) {
+            throw new BadRequestException("Event with ID " + eventId + " not found.");
+        }
 
+        Event event = eventOptional.get();
+        Optional<Registration> registrationOptional = event.getRegistrations().stream()
+                .filter(registration -> registration.getId().equals(registrationId))
+                .findFirst();
+
+        if (registrationOptional.isEmpty()) {
+            throw new BadRequestException("Sorry, but registration with ID " + registrationId + " is not found in the event.");
+        }
+
+        Registration registration = registrationOptional.get();
+        int scoreIncrement = parseScore(manualScoreDto.getScore());
+
+        // Updating score in Registration
+        int currentScore = registration.getScore();
+        registration.setScore(currentScore + scoreIncrement);
+
+        // Saving ManualScore
+        ManualScore manualScore = new ManualScore();
+        manualScore.setManualScore(manualScoreDto.getScore());
+        manualScore.setComment(manualScoreDto.getComment());
+        manualScore.setRegistration(registration);
+        registration.getManualScores().add(manualScore);
+
+        // Saving changes
+        eventRepository.save(event);
+    }
+
+    private int parseScore(String score) {
+        try {
+            return Integer.parseInt(score);
+        } catch (NumberFormatException e) {
+            throw new BadRequestException("Invalid score format. Score must be an integer.");
+        }
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
