9ab368584f81128a9d3bd00d963b2d495fb60284
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-04 10:45:06 +0200

    Deleting imports

--- src/main/java/hr/donata/eventinnodemo/mapper/ManualScoreMapper.java
+++ src/main/java/hr/donata/eventinnodemo/mapper/ManualScoreMapper.java
@@ -1,7 +1,17 @@
 package hr.donata.eventinnodemo.mapper;
 
+
+import hr.donata.eventinnodemo.dto.NameDto;
+import hr.donata.eventinnodemo.entity.Name;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ManualScoreMapper {
+
+    ManualScoreMapper INSTANCE = Mappers.getMapper(ManualScoreMapper.class);
+
+    Name nameDtoToName(NameDto nameDto);
+
+
 }
