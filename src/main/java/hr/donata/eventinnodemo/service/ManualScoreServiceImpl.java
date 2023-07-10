86796d4b5328b34a69ab6a71ca40a367375842cd
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-10 12:28:12 +0200

    Adding annotations

---
+++ src/main/java/hr/donata/eventinnodemo/service/ManualScoreServiceImpl.java
@@ -0,0 +1,12 @@
+package hr.donata.eventinnodemo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+
+public class ManualScoreServiceImpl implements ManualScoreService {
+}
