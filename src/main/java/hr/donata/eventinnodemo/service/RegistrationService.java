c83ab70916950be2c0e22678ddce905fec6b5581
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-10 12:31:00 +0200

    Editing methods

--- src/main/java/hr/donata/eventinnodemo/service/RegistrationService.java
+++ src/main/java/hr/donata/eventinnodemo/service/RegistrationService.java
@@ -2,12 +2,11 @@ package hr.donata.eventinnodemo.service;
 
 import hr.donata.eventinnodemo.dto.ManualScoreDto;
 import hr.donata.eventinnodemo.dto.RegistrationDto;
-import hr.donata.eventinnodemo.entity.Registration;
 import org.springframework.http.ResponseEntity;
 
 public interface RegistrationService {
     ResponseEntity<String> create(RegistrationDto registrationDto, Long eventId);
     void deleteRegistration(Long id);
     void deleteRegistrationForEvent(Long registrationId, Long eventId);
-    ResponseEntity<Registration> scoreRegistration(Long registrationId, ManualScoreDto manualScore);
+    ResponseEntity<RegistrationDto> scoreRegistration(Long registrationId, ManualScoreDto manualScore);
 }
