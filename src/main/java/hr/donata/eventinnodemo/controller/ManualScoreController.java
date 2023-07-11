94f27b5ea9923bfda98149551d616c6efd3e08e7
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-11 14:36:53 +0200

    Adding POST mapping

--- src/main/java/hr/donata/eventinnodemo/controller/ManualScoreController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/ManualScoreController.java
@@ -1,11 +1,26 @@
 package hr.donata.eventinnodemo.controller;
 
+import hr.donata.eventinnodemo.dto.ManualScoreDto;
+import hr.donata.eventinnodemo.dto.NameDto;
+import hr.donata.eventinnodemo.service.ManualScoreService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/manualScore")
 public class ManualScoreController {
+    private final ManualScoreService manualScoreService;
+    public ManualScoreController(ManualScoreService manualScoreService) {
+        this.manualScoreService = manualScoreService;
+    }
 
+    @PostMapping(path = "/save")
+    private ResponseEntity<ManualScoreDto> saveName(@RequestBody ManualScoreDto manualScoreDto) {
+        manualScoreService.create(manualScoreDto);
+        return ResponseEntity.ok(manualScoreDto);
+    }
 
 }
