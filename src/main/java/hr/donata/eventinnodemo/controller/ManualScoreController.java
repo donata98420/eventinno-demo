22ba96e5e2cd9132d9d0e37fd614d10dccd60daf
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-11 14:21:54 +0200

    Adding annotations

--- src/main/java/hr/donata/eventinnodemo/controller/ManualScoreController.java
+++ src/main/java/hr/donata/eventinnodemo/controller/ManualScoreController.java
@@ -1,4 +1,11 @@
 package hr.donata.eventinnodemo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/manualScore")
 public class ManualScoreController {
+
+
 }
