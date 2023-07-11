b90dae1662a778b676d17a9d27ef7fc426da38f2
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-11 13:44:02 +0200

    Adding create method

--- src/main/java/hr/donata/eventinnodemo/service/ManualScoreServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/ManualScoreServiceImpl.java
@@ -2,7 +2,10 @@ package hr.donata.eventinnodemo.service;
 
 import hr.donata.eventinnodemo.dto.ManualScoreDto;
 import hr.donata.eventinnodemo.entity.Event;
+import hr.donata.eventinnodemo.entity.ManualScore;
 import hr.donata.eventinnodemo.entity.Registration;
+import hr.donata.eventinnodemo.mapper.ManualScoreMapper;
+import hr.donata.eventinnodemo.repository.ManualScoreRepository;
 import hr.donata.eventinnodemo.repository.RegistrationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,54 +15,21 @@ import java.util.Optional;
 
 @Service
 @Transactional
-@RequiredArgsConstructor
-
 public class ManualScoreServiceImpl implements ManualScoreService {
-    private final RegistrationRepository registrationRepository;
-
-    public ResponseEntity manualScoreRegistration(Long registrationId, Long eventId, ManualScoreDto manualScoreDto) {
-
-        // Checking registration and event (+ exception)
-        Optional<Registration> registrationOptional = registrationRepository.findById(registrationId);
-        Registration registration = registrationOptional.orElseThrow(() -> new IllegalArgumentException("Registration not found."));
-
-        Event event = registration.getEvent();
-        if (event == null || event.getId() == null || !event.getId().equals(eventId)) {
-            throw new IllegalArgumentException("Sorry, but this registration is not assigned to this event.");
-        }
-
-        // Checking scoring - addition or subtraction + exception
-        boolean isAddition = manualScoreDto.isAddition();
-        int scoringValue = manualScoreDto.getValue();
-
-        // Set the updated score
-        setScore(registration, isAddition, scoringValue);
-
-        // Saving the score
-        saveScore(registration);
-        return ResponseEntity.ok(registration);
+    public ManualScoreServiceImpl(ManualScoreRepository manualScoreRepository, ManualScoreMapper manualScoreMapper) {
+        this.manualScoreRepository = manualScoreRepository;
+        this.manualScoreMapper = manualScoreMapper;
     }
 
-    private ResponseEntity<Integer> setScore(Registration registration, boolean isAddition, int scoringValue) {
+    private final ManualScoreRepository manualScoreRepository;
+    private final ManualScoreMapper manualScoreMapper;
 
-        // Getting the current score
-        int currentScore = registration.getScore();
 
-        // Updating the score + / -
-        int updatedScore;
-        if (isAddition) {
-            updatedScore = currentScore + scoringValue;
-        } else {
-            updatedScore = currentScore - scoringValue;
-        }
-
-        // Setting and returning the updated score
-        registration.setScore(updatedScore);
-        return ResponseEntity.ok(updatedScore);
-    }
-
-    private void saveScore(Registration registration) {
-        registrationRepository.save(registration);
+    @Override
+    public ManualScoreDto create(ManualScoreDto manualScoreDto) {
+        ManualScore manualScore = manualScoreMapper.manualScoreDtoToManualScore(manualScoreDto);
+        manualScoreRepository.save(manualScore);
+        return manualScoreDto;
     }
 }
 
