ddb005c6a265e574a6681ac6a7e04be8dc651fd8
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-11 11:45:57 +0200

    Removing imports

--- src/main/java/hr/donata/eventinnodemo/service/ManualScoreServiceImpl.java
+++ src/main/java/hr/donata/eventinnodemo/service/ManualScoreServiceImpl.java
@@ -1,10 +1,9 @@
 package hr.donata.eventinnodemo.service;
 
 import hr.donata.eventinnodemo.dto.ManualScoreDto;
+import hr.donata.eventinnodemo.dto.RegistrationDto;
 import hr.donata.eventinnodemo.entity.Event;
 import hr.donata.eventinnodemo.entity.Registration;
-import hr.donata.eventinnodemo.mapper.RegistrationMapper;
-import hr.donata.eventinnodemo.repository.EventRepository;
 import hr.donata.eventinnodemo.repository.RegistrationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +17,9 @@ import java.util.Optional;
 @RequiredArgsConstructor
 
 public class ManualScoreServiceImpl implements ManualScoreService {
-
     private final RegistrationRepository registrationRepository;
-    private final RegistrationMapper registrationMapper;
-    private final EventRepository eventRepository;
 
-    public ResponseEntity scoreRegistration(Long registrationId, Long eventId, ManualScoreDto manualScoreDto) {
+    public ResponseEntity<RegistrationDto> manualScoreRegistration(Long registrationId, Long eventId, ManualScoreDto manualScoreDto) {
 
         // Checking registration and event (+ exception)
         Optional<Registration> registrationOptional = registrationRepository.findById(registrationId);
@@ -39,37 +35,35 @@ public class ManualScoreServiceImpl implements ManualScoreService {
         int scoringValue = manualScoreDto.getValue();
 
         // Set the updated score
-        setScore(registration, isAddition, scoringValue);
+        private void setScore setScore(registration, isAddition, scoringValue);
 
         // Saving the score
-        saveScore(registration);
+        private void saveScore saveScore(registration);
 
-        return ResponseEntity.ok(registration);
-    }
 
-    private void setScore(Registration registration, boolean isAddition, int scoringValue) {
-        // Getting the current score
-        int currentScore = registration.getScore();
+        (Registration registration,boolean isAddition, int scoringValue){
+            // Getting the current score
+            int currentScore = registration.getScore();
 
-        // Updating the score
-        int updatedScore;
-        if (isAddition) {
-            updatedScore = currentScore + scoringValue;
-        } else {
-            updatedScore = currentScore - scoringValue;
+            // Updating the score
+            int updatedScore;
+            if (isAddition) {
+                updatedScore = currentScore + scoringValue;
+            } else {
+                updatedScore = currentScore - scoringValue;
+            }
+
+            // Setting the updated score
+            registration.setScore(updatedScore);
         }
 
-        // Setting the updated score
-        registration.setScore(updatedScore);
+        (Registration registration){
+            registrationRepository.save(registration);
+        }
     }
 
-    private void saveScore(Registration registration) {
-        registrationRepository.save(registration);
-    }
-}
 
 
-}
 
 
 
