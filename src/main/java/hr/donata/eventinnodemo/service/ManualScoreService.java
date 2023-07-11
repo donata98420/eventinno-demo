14b80a3e7b3d36a037b63db8672578bcb24f66a3
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-11 13:43:31 +0200

    Adding create method

--- src/main/java/hr/donata/eventinnodemo/service/ManualScoreService.java
+++ src/main/java/hr/donata/eventinnodemo/service/ManualScoreService.java
@@ -1,6 +1,9 @@
 package hr.donata.eventinnodemo.service;
 
+import hr.donata.eventinnodemo.dto.ManualScoreDto;
+
 public interface ManualScoreService {
 
 
+    ManualScoreDto create(ManualScoreDto manualScoreDto);
 }
