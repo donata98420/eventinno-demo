b03dfc9219448d33a630ae23c1f313032e356aa1
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-11 13:45:00 +0200

    Adding ManualScoreDto

--- src/main/java/hr/donata/eventinnodemo/mapper/ManualScoreMapper.java
+++ src/main/java/hr/donata/eventinnodemo/mapper/ManualScoreMapper.java
@@ -1,5 +1,6 @@
 package hr.donata.eventinnodemo.mapper;
 
+import hr.donata.eventinnodemo.dto.ManualScoreDto;
 import hr.donata.eventinnodemo.entity.ManualScore;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,7 +11,9 @@ public interface ManualScoreMapper {
 
     ManualScoreMapper INSTANCE = Mappers.getMapper(ManualScoreMapper.class);
     @Mapping(target = "id", ignore = true)
-    ManualScore manualScoreDtoToManualScore(ManualScore manualScore);
+    ManualScore manualScoreDtoToManualScore(ManualScoreDto manualScoreDto);
+
+
 
 
 
