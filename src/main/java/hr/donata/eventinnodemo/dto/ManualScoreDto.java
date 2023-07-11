6cc7ce37077a3588b9ad64aaf70e4faf57a9023a
Donata Szombathelyi <74142933+donnata98@users.noreply.github.com>
2023-07-11 12:35:39 +0200

    Adding new entity

--- src/main/java/hr/donata/eventinnodemo/dto/ManualScoreDto.java
+++ src/main/java/hr/donata/eventinnodemo/dto/ManualScoreDto.java
@@ -7,8 +7,13 @@ public class ManualScoreDto {
 
     private String score;
     private String comment;
+    private int value;
 
     public boolean isAddition() {
         return score != null && score.equalsIgnoreCase("addition");
+    }
+
+    public int getValue() {
+        return value;
     }
 }
